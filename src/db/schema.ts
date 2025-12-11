ac413a3af1329e82a568edf2956d83816b4ff143
@油腻_w4v0 <16615812538@163.com>
2025-12-11 13:00:44 +0800

    添加对部门架构设计的schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,27 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { boolean, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
 
 export const todos = pgTable('todos', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
   createdAt: timestamp('created_at').defaultNow(),
 })
+
+export const departments = pgTable('departments', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  parentId: integer('parent_id'),
+
+  isActive: boolean('is_active').default(true),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
+})
+
+export const departmentsRelations = relations(departments, ({ many}) => ({
+  parent: many(departments, {
+    relationName: 'parentDepartments',
+  }),
+  children: many(departments, {
+    relationName: 'childrenDepartments',
+  }),
+}))
